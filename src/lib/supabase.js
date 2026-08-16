6bf363f07a264d5c76e1d81a7cbe6138afa92a24
nguru-nn <njooro@gmail.com>
2026-08-16 17:01:50 +0300

    Delete src/lib/supabase.js

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://dygjmgehmdxxumgqzfyo.supabase.co'
-const supabaseAnonKey = 'sb_publishable_uLAOzFmpIbPAPxIbprXjsQ_xldUOLSV'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
