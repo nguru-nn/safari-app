360f206548d8cbfb1889094e72621b82e3439e57
nguru-nn <njooro@gmail.com>
2026-08-23 22:23:10 +0300

    Change import from react-router-dom to react-router

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router'
 import { IconLayoutGrid, IconMap, IconUsers, IconLogout } from '@tabler/icons-react'
 import { useAuth } from '../contexts/AuthContext'
 
